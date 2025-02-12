2f9464a771a3245691d4d084134879242e370faa
moneyandjelly <142682399+moneyandjelly@users.noreply.github.com>
2025-02-12 14:26:14 +0900

    feat: 호스트 요청 허용 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['geunlee.online'], // Add your host here
+  },
 })
